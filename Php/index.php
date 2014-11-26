2fe3f7edeea436f139b3dd045ab9348ace856cc4
Jérôme Aviles <avijer@hotmail.fr>
2014-11-26 14:45:22 +0100

    Implementation base/connect/fonctions de sel/up/alt/...

--- Php/index.php
+++ Php/index.php
@@ -7,8 +7,9 @@ $EX = isset($_REQUEST['EX']) ? $_REQUEST['EX'] : 'home';
 
 switch($EX)
 {
-	case 'home'     : home();     break;
-	case 'login'     : login();     break;
+	case 'home'      : home();       break;
+	case 'login'     : login();      break;
+    case 'reportList': reportList(); break;
 	default : error();
 }
 
@@ -36,11 +37,20 @@ function error()
 
 function login()
 {
-	global $page;
-	$page['title'] = 'Erreur 404 !';
-	$page['class'] = 'VHtml';
-	$page['method'] = 'showHtml';
-	$page['arg'] = '../Html/unknown.php';
+    global $page;
+    $page['title'] = 'Erreur 404 !';
+    $page['class'] = 'VHtml';
+    $page['method'] = 'showHtml';
+    $page['arg'] = '../Html/unknown.php';
+}
+
+function reportList()
+{
+    global $page;
+    $page['title'] = 'Liste des rapports';
+    $page['class'] = 'VHtml';
+    $page['method'] = 'showHtml';
+    $page['arg'] = '../Html/reportlist.php';
 }
 ?>
 
--- View/layout.view.php
+++ View/layout.view.php
@@ -9,7 +9,10 @@ $vpage = new $page['class']();
 <head>
     <meta charset="utf-8" />
     <title><?= $page['title']; ?></title>
+    <link href="../Css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../Css/main.css" />
+    <link rel="stylesheet" type="text/css" href="../Css/bootstrap.min.css" />
+
 </head>
 <body>
     <?php $vUserInfo->showUserInfo() ?>
@@ -19,4 +22,8 @@ $vpage = new $page['class']();
     <div class="page">
         <?php $vpage->$page['method']($page['arg']) ?>
     </div>
+    <!-- jQuery <3 ! -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../Js/bootstrap.min.js"></script>
 </body>
