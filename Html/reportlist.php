b9cf5e067693cb394c3861b033ac1a88b67e8104
Jérôme Aviles <avijer@hotmail.fr>
2014-11-26 14:49:38 +0100

    Jerome > debut list rapports

---
+++ Html/reportlist.php
@@ -0,0 +1,14 @@
+<h1>Liste des rapports</h1>
+<div class="report_filters">
+    <form class="well well-sm text-center">
+        <label for="report_filter_profile">Profils</label><input name="radioreport" id="report_filter_profile" type="radio" value="report_profile"/>
+        <label for="report_filter_article">Article</label><input name="radioreport" id="report_filter_article" type="radio" value="report_article"/>
+        <label for="report_filter_message">Message</label><input name="radioreport" id="report_filter_message" type="radio" value="report_message"/>
+    </form>
+</div>
+<div class="result_reports well well-lg">
+    <h2 class="titre article">Deuxième article</h2>
+    <div class="auteur">par Auteur (Association)</div>
+    <p> Nam quibusdam, quos audio sapientes habitos in Graecia, placuisse opinor mirabilia quaedam (sed nihil est quod illi non persequantur argutiis): partim fugiendas esse nimias amicitias, ne necesse sit unum sollicitum esse pro pluribus; satis superque esse sibi suarum cuique rerum, alienis nimis implicari molestum esse; commodissimum esse quam laxissimas habenas habere amicitiae, quas vel adducas, cum velis, vel remittas; caput enim esse ad beate vivendum securitatem, qua frui non possit animus, si tamquam parturiat unus pro pluribus. [...]</p>
+    <a class="lienarticle">Voir l'article</a>
+</div>
