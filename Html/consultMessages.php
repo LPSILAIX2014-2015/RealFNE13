df54585886a7cd93924bd43de5aa3d502afb8c72
Gregp13 <gregp1365@gmail.com>
2014-12-10 17:04:01 +0100

    Ajout liste messages

---
+++ Html/consultMessages.php
@@ -0,0 +1,18 @@
+<div class="col-md-6">
+	
+	<div class="table-responsive">
+		<table class="table table-hover">
+			<thead>
+				<tr>
+					<th>Envoyé par</th>
+					<th>Titre</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php global $content_messages; echo $content_messages; ?>
+			</tbody>
+		</table>
+	</div>
+</div>
+
--- View/VConsultMessages.view.php
+++ View/VConsultMessages.view.php
@@ -7,8 +7,39 @@ class VConsultMessages
   
   public function showConsultMessages($path)
   {
+  	//Simulation de marchage
+  	//$idUser = $_SESSION['idUser'];
+  	$idUser = 2;
+
+  	global $connec;
+    
+
+    $state = $connec->prepare("SELECT MESSAGES.*, USER.NAME SENDER_NAME, USER.SURNAME SENDER_SURNAME  FROM MESSAGES, USER WHERE RECEIVER_ID = :idUser AND MESSAGES.SENDER_ID = USER.ID");
+    $state->bindValue('idUser', $idUser, PDO::PARAM_INT);
+    $state->execute();    
+    $data_messages = $state->fetchAll(PDO::FETCH_ASSOC);
   	
-  	
+    /* REMPLISSAGATION DU CONTENU */
+
+
+    global $content_messages;
+    $content_messages = "";
+
+    for($i = 0 ; $i < count($data_messages) ; ++$i)
+    {
+    	$content_messages .= '<tr>';
+
+		$content_messages .= '<td>'.$data_messages[$i]['SENDER_NAME'].' '.$data_messages[$i]['SENDER_SURNAME'].'</td>';    	
+		$content_messages .= '<td>'.$data_messages[$i]['TITLE'].'</td>';    	
+		$content_messages .= '<td><a class="btn-sm btn-warning"><i class="glyphicon glyphicon-plus"></i></a></td>';    	
+
+
+    	$content_messages .= '<tr />';
+    }
+
+
+    /* REMPLISSAGATION DU CONTENU */
+
     $vhtml = new VHtml();
     $vhtml->showHtml($path);
     
