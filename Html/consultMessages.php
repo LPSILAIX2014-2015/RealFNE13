4486351b73f129a29c097ca5ac2a67369ce877cf
Gregp13 <gregp1365@gmail.com>
2015-01-28 15:46:16 +0100

    Ajout catégorie et theme

--- Html/consultMessages.php
+++ Html/consultMessages.php
@@ -1,24 +1,63 @@
 <?php global $content_messages; ?>
+<?php global $content_messages_archive; ?>
+
 <div class="panel panel-default">
-  <div class="panel-heading">Messages</div>
-  <div class="panel-body">
-    <div class="table-responsive">
-		<table class="tableMessages table table-hover">
-			<thead>
-				<tr>
-					<th class="SENDER_MESSAGE">Envoyé par</th>
-					<th class="TITLE_MESSAGE">Objet</th>
-					<th class="DATE_MESSAGE">Date</th>
-					<th class="BUTTON_MESSAGE"></th>
-				</tr>
-			</thead>
-			<tbody>
-				<?= $content_messages; ?>
-			</tbody>
-		</table>
+	<div class="panel-body">
+		<button type="button" data-bool="1" class="displayArchive btn btn-success">Afficher les messages archivés</button>
 	</div>
-  </div>
-  <script type="text/javascript" src="Js/showMessage.js"></script>
 </div>
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Messages</h3>
+	</div>
+	<div class="panel-body">
+		<div class="table-responsive">
+			<table class="tableMessages table table-hover">
+				<thead>
+					<tr>
+						<th class="SENDER_MESSAGE">Envoyé par</th>
+						<th class="TITLE_MESSAGE">Objet</th>
+						<th class="CATEGORY_MESSAGE">Catégorie</th>
+						<th class="THEME_MESSAGE">Thème</th>
+						<th class="DATE_MESSAGE">Date</th>
+						<th class="BUTTON_MESSAGE"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?= $content_messages; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+<div class="panel panel-default panelArchiveMessages">
+	<div class="panel-heading">
+		<h3 class="panel-title">Messages archivés</h3>
+	</div>
+	<div class="panel-body">
+		<div class="table-responsive">
+			<table class="tableMessages table table-hover">
+				<thead>
+					<tr>
+						<th class="SENDER_MESSAGE">Envoyé par</th>
+						<th class="TITLE_MESSAGE">Objet</th>
+						<th class="CATEGORY_MESSAGE">Catégorie</th>
+						<th class="THEME_MESSAGE">Thème</th>
+						<th class="DATE_MESSAGE">Date</th>
+						<th class="BUTTON_MESSAGE"></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?= $content_messages_archive; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript" src="Js/showMessage.js"></script>
 
 
