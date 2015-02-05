e6f83722bd1344844c690349c8a47e05f9f913ac
Jérôme Aviles <avijer@hotmail.fr>
2015-02-05 13:17:37 +0100

    Correction affichage articles recents

--- Html/recentarticles.php
+++ Html/recentarticles.php
@@ -5,31 +5,23 @@
  */
 
 $sql = new MDBase();
-$query = 'select ID, TITLE, THEME_ID  from POST order by PDATE desc limit 0,5;' ;
-$result = $sql->query($query);
-if ($result->rowCount() > 0) {
-    $tab_themes = array(1 => 'transport-mobilite-durable.png',
-                        2 => 'mission-juridique.png',
-                        3 => 'climat-air-energie.png',
-                        4 => 'sante-et-environnement.png',
-                        5 => 'amenagement-durable-urbanisme.png',
-                        6 => 'industrie.png',
-                        7 => 'eau-milieu-naturel.png',
-                        8 => 'agriculture.png'
-                        );
+$query = $sql->prepare("select POST.ID, TITLE, NAME, THEME.IMAGEPATH AS IMG
+from POST, THEME
+where PTYPE='ARTICLE' AND STATUS > 0
+AND THEME_ID = THEME.ID
+order by PDATE desc limit 0,5;") ;
+$query->execute();
 ?>
 <div class="recentarticles">
     <h5 class="titrecol">Derniers articles</h5>
     <ul>
         <?php
-        while ($row = $result->fetch()) {
-            if ($row['THEME_ID'] != 0) { $icone = 'Img/iconesthemes/'.$tab_themes[$row['THEME_ID']]; }
+
+        while ($row = $query->fetch()) {
+            if ($row['IMG'] != NULL) { $icone = 'Img/iconesthemes/'.$row['IMG']; }
             else { $icone = 'Img/pastille.png'; }
             echo '<li><span class="iconesarticle"><img src="'.$icone.'"></span><span class="titrearticle">'.$row['TITLE'].'</span></li>' ;
         }
         ?>
     </ul>
 </div>
-<?php
-}
-?>
