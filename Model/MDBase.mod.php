c97fa68eb847e0ee54d2d58ac4a8a7cf088d7a99
Jérôme Aviles <avijer@hotmail.fr>
2015-02-11 08:37:37 +0100

    Add DB OVH

--- Model/MDBase.mod.php
+++ Model/MDBase.mod.php
@@ -4,12 +4,22 @@ class MDBase extends PDO {
 
 
     private static $engine = 'mysql';
+
+    // OVH
+    private static $dbName = 'laplateftifne13' ;
+    private static $dbHost = 'laplateftifne13.mysql.db' ;
+    private static $dbUsername = 'laplateftifne13';
+    private static $dbUserPassword = 'natureC13';
+    private static $cont  = null;
+
+    /*
+    // LOCAL
     private static $dbName = 'FNESITE' ;
     private static $dbHost = 'localhost' ;
     private static $dbUsername = 'root';
     private static $dbUserPassword = 'mysql';
     private static $cont  = null;
-
+*/
     public function __construct(){
         $dns = self::$engine.':dbname='.self::$dbName.";host=".self::$dbHost;
         parent::__construct( $dns, self::$dbUsername, self::$dbUserPassword );
