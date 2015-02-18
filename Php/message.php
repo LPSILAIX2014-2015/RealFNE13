e438753b2490d14737e0c674cb094ec70ba9c873
Nofixa <baptiste.dejean1@gmail.com>
2015-02-18 14:15:48 +0100

    METTEZ LES VARIABLES SQL EN MAJUSCULES MERCI

--- Php/message.php
+++ Php/message.php
@@ -14,7 +14,7 @@ $senderId = $_SESSION['ID_USER'];
 $nb_message = $_POST['nbmessages'];
 $receiver1 = $_POST['receiver1'];
 
-$request1 = $pdo -> prepare("SELECT id as id FROM USER WHERE login = ?");
+$request1 = $pdo -> prepare("SELECT ID as id FROM USER WHERE LOGIN = ?");
 $request1 -> execute(array($receiver1));
 $req = $request1->fetch();
 if ($nb_message < 100)
@@ -36,20 +36,20 @@ if ($nb_message < 100)
         $theme = $_POST['theme'];
         $cat = $_POST['category'];
 
-        $request2 = $pdo->prepare("INSERT INTO message(ID,SENDER_ID,RECEIVER_ID,CAT_ID,THEME_ID,ISREAD,SENDDATE,TITLE,CONTENT) VALUES ('',?,?,?,?,0,CURRENT_DATE,?,?)");
+        $request2 = $pdo->prepare("INSERT INTO MESSAGE(ID,SENDER_ID,RECEIVER_ID,CAT_ID,THEME_ID,ISREAD,SENDDATE,TITLE,CONTENT) VALUES ('',?,?,?,?,0,CURRENT_DATE,?,?)");
         $request2->execute(array($senderId,$receiverId,$cat,$theme,$title,$content));
 
-        $request4 = $pdo -> prepare("SELECT SURNAME as prenom, NAME as nom FROM USER WHERE id = ?");
+        $request4 = $pdo -> prepare("SELECT SURNAME as prenom, NAME as nom FROM USER WHERE ID = ?");
         $request4 -> execute(array($senderId));
         $req4 = $request4->fetch();
 
-        $request5 = $pdo -> prepare("SELECT SURNAME as prenom1, NAME as nom1 FROM USER WHERE id = ?");
+        $request5 = $pdo -> prepare("SELECT SURNAME as prenom1, NAME as nom1 FROM USER WHERE ID = ?");
         $request5 -> execute(array ($receiverId));
         $req5 = $request5->fetch();
 
         $rapport = $req4['prenom'].' '.$req4['nom'].' a contacte '.$req5['prenom1'].' '.$req5['nom1'].'. ';
 
-        $request3 = $pdo->prepare("INSERT INTO report(ID,RDATE,TYPE,CONTENT) VALUES('',CURRENT_DATE,?,?)");
+        $request3 = $pdo->prepare("INSERT INTO REPORT(ID,RDATE,TYPE,CONTENT) VALUES('',CURRENT_DATE,?,?)");
         $request3->execute(array('MESSAGE',$rapport));
         echo "3";
     }
