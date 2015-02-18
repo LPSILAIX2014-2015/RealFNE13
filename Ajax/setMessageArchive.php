732545438601430adba905317d862f111bed3637
Nofixa <baptiste.dejean1@gmail.com>
2015-02-18 14:34:54 +0100

    Repare Notif Archive

--- Ajax/setMessageArchive.php
+++ Ajax/setMessageArchive.php
@@ -4,7 +4,7 @@
 
 	$state = $database->prepare("UPDATE MESSAGE SET ISARCHIVE = :yes WHERE ID = :id");
     $state->bindValue('id', htmlspecialchars($_GET['id']), PDO::PARAM_INT);
-    $state->bindValue('yes', '1', PDO::PARAM_INT);
+    $state->bindValue('yes', 1, PDO::PARAM_INT);
     $state->execute();
 
 	echo json_encode(true);
