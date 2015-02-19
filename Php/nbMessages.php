22ac783e2ae8be8ca412ae23145d790ca1e7fbae
Nofixa <baptiste.dejean1@gmail.com>
2015-02-19 11:06:37 +0100

    correction nbMessages.php

--- Php/nbMessages.php
+++ Php/nbMessages.php
@@ -9,20 +9,11 @@ $pdo = new MDBase();
 
 $senderId = $_SESSION['ID_USER'];
 
-
-<<<<<<< HEAD
 $requete7 = $pdo->prepare("SELECT ROLE as role from USER WHERE ID = ?");
 $requete7 -> execute(array($senderId));
 $reqt7 = $requete7->fetch();
 
 $request7 = $pdo->prepare("SELECT ASSOCIATION_ID as assoc FROM USER where ID = ?");
-=======
-$requete7 = $pdo->prepare("SELECT ROLE as role from USER WHERE id = ?");
-$requete7 -> execute(array($senderId));
-$reqt7 = $requete7->fetch();
-
-$request7 = $pdo->prepare("SELECT association_id as assoc FROM USER where id = ?");
->>>>>>> d08d8ac59e83787e7b7aa2b40a4945730641cebe
 $request7 -> execute(array($senderId));
 $req7 = $request7->fetch();
 
