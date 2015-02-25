51e8c7613c3dc589c46585e3b742aceb0ff61280
Rose19 <catherine.farrahifar@gmail.com>
2015-02-25 13:58:33 +0100

    localhost pour moi (Cathy)

--- Model/MDBase.mod.php
+++ Model/MDBase.mod.php
@@ -9,17 +9,23 @@ class MDBase extends PDO {
     private static $dbHost = 'localhost' ;
     private static $dbUsername = 'root';
     private static $dbUserPassword = '';
+    private static $cont  = null;*/
+
+    //Site local Cathy
+    private static $dbName = 'FNESITE' ;
+    private static $dbHost = 'localhost' ;
+    private static $dbUsername = 'root';
+    private static $dbUserPassword = 'mysql';
     private static $cont  = null;
-*/
 
     //Site DEV
-
+/*
     private static $dbName = 'fnekxazadev' ;
     private static $dbHost = 'mysql51-84.pro' ;
     private static $dbUsername = 'fnekxazadev';
     private static $dbUserPassword = 'natureC13';
     private static $cont  = null;
-
+*/
     public function __construct(){
         $dns = self::$engine.':dbname='.self::$dbName.";host=".self::$dbHost;
         parent::__construct( $dns, self::$dbUsername, self::$dbUserPassword );
