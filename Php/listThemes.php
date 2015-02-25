78ef4d1c1a09b0d2076f0a9b44ec730198d69af3
Rémi A. <arnouxr@hotmail.fr>
2015-02-25 13:31:58 +0100

    Correction formulaire message
    ..où les options de valeur 1 n'apparaissaient pas

--- Php/listThemes.php
+++ Php/listThemes.php
@@ -8,7 +8,7 @@ $pdo = new MDBase();
 
 $request11 = $pdo -> query("select NAME as themes from theme");
 $row1 = $request11->fetchAll();
-$var = 1;
+$var = 0;
 $taille = count($row1);
 echo "<select id='theme'>";
 if (isset ($_SESSION["theme"]))
@@ -16,13 +16,14 @@ if (isset ($_SESSION["theme"]))
 	echo "<option value='0'>Pas de thématique sélectionnée</option>";
 	while ($var<$taille)
 	{
-		if ($var == $_SESSION["theme"])
+		$a = $var+1;
+		if ($var == $_SESSION["theme"]-1)
 		{
-			echo "<option value='".$var."' selected>".$row1[$var]['themes']."</option>";
+			echo "<option value='".$a."' selected>".$row1[$var]['themes']."</option>";
 		}
 		else
 		{
-			echo "<option value='".$var."'>".$row1[$var]['themes']."</option>";	
+			echo "<option value='".$a."'>".$row1[$var]['themes']."</option>";	
 		}
 		$var = $var+1;
 	}
@@ -32,7 +33,8 @@ else
 	echo "<option value='0'>Pas de thématique sélectionnée</option>";
 	while($var<$taille)
 	{
-		echo "<option value='".$var."'>".$row1[$var]['themes']."</option>";
+		$a = $var+1;
+		echo "<option value='".$a."'>".$row1[$var]['themes']."</option>";
 		$var = $var+1;
 	}
 }
