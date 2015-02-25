eb869d9fb78305e575b11fe6dc57b01969c83289
Rose19 <catherine.farrahifar@gmail.com>
2015-02-25 13:57:23 +0100

    seul la modif du status rajouté

--- View/VShowArticle.view.php
+++ View/VShowArticle.view.php
@@ -1,6 +1,7 @@
 <?php
 class VShowArticle
 {
+
 	public function __construct(){}
 	
 	public function __destruct(){}
@@ -18,11 +19,11 @@ class VShowArticle
 		{
 			$state = $connec->prepare(
 				'SELECT U.ID
-				FROM USER U, ASSOCIATION A, POST P
-				WHERE WRITER_ID = U.ID
-				AND A.ID = :idA
-				AND U.ASSOCIATION_ID = A.ID'
-				);
+				 FROM   USER U, ASSOCIATION A, POST P
+				 WHERE  WRITER_ID = U.ID
+           AND  A.ID = :idA
+           AND  U.ASSOCIATION_ID = A.ID'
+	   	);
 			$state->bindValue('idA', htmlspecialchars($_GET['idA']), PDO::PARAM_INT);
 			$state->execute();
 			$data_idUserList = $state->fetchAll(PDO::FETCH_ASSOC);
@@ -32,14 +33,14 @@ class VShowArticle
 			$data_idUserList = false;
 		}
 
-
 		// AFFICHAGE
 		$state = $connec->prepare(
 			"SELECT P.*, DATE_FORMAT(P.PDATE, '%d/%m/%Y') AS PDATE,
 			U.NAME, U.SURNAME, U.ASSOCIATION_ID ASSOC_ID
 			FROM   POST P, USER U
 			WHERE  P.WRITER_ID = U.ID
-			AND  P.PTYPE = 'ARTICLE'
+			  AND  P.PTYPE = 'ARTICLE'
+        AND  P.STATUS > 0
 			ORDER BY id DESC"
 			);
 		$state->execute();
@@ -54,6 +55,6 @@ class VShowArticle
 		$vhtml->showHtml($_html);
 
 	} // showShowArticle($_html)
-
+  
 } // VHtml
 ?>
