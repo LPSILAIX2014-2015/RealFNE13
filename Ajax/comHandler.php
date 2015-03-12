f0f7e8b277ca8055a1ca3db14a17509683c02367
h1103402 <jonathh@hotmail.fr>
2015-03-12 15:21:47 +0100

    refonte des requetes sur les quetes

--- Ajax/comHandler.php
+++ Ajax/comHandler.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+
 	require_once('../Model/MDBase.mod.php');
 	require_once('../Model/MCommentaire.mod.php');
 
@@ -8,12 +9,22 @@
 	{
 		case 'getCommentaire' :
 			$result = $mcom->getCommentairePost($_POST['idPost']);
-			$jsonarray = array("com" => $result);
+			$jsonarray = array("com" => $result, "session" => $_SESSION);
 			echo json_encode($jsonarray);
 			break;
 		case 'addCommentaire' :
 			$result = $mcom->addCommentairePost($_POST['idPost'], $_POST['content']);
 			$jsonarray = array("isInsert" => $result);
+			echo json_encode($jsonarray);
+			break;
+		case 'deleteCommentaire' :
+			$result = $mcom->deleteCommentaire($_POST['idcom']);
+			$jsonarray = array("deleted" => $result);
+			echo json_encode($jsonarray);
+			break;
+		case 'updateCommentaire' :
+			$result = $mcom->updateCommentaire($_POST['idcom'], $_POST['content']);
+			$jsonarray = array("updated" => $result);
 			echo json_encode($jsonarray);
 			break;
 	}
--- Js/commentaire.js
+++ Js/commentaire.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 	});
 
 	getCommentaire();	
+
 });
 
 
@@ -24,15 +25,24 @@ function getCommentaire(){
 		success: function(response) {
 			$('#divCom').empty();
 			for(i in response.com){
-				currentComId = 'com'+ response.com[i]['ID'];
+
+				var idcom = response.com[i]['ID'];
+				var currentComId = 'com'+idcom ;
 
 				$('#divCom').append('<div id="'+ currentComId +'" class="com">');
 				$('#'+currentComId).append('<span class="nameCom">'+ response.com[i]['NAME'] + ' ' + response.com[i]['SURNAME'] +'</span>'
-					+'<span class="dateCom">'+ response.com[i]['COM_DATE'] +'</span>');
+					 +'<span class="dateCom">'+ response.com[i]['COM_DATE'] +'</span>');
 				$('#'+currentComId).append('<hr class="sepCom">');
-				$('#'+currentComId).append('<div class="contentCom">'+ response.com[i]['CONTENT'] +'</div>');
+				$('#'+currentComId).append('<div id="content'+idcom+'" class="contentCom">'+ response.com[i]['CONTENT'] +'</div>');
 				$('#'+currentComId).append('</div>');
-				$('#'+currentComId).append('<button>Supprimer</button>');
+
+				//The person who post the com can modify his it. admins and the sysadmin can modify and delete all coms
+				if (response.session['ROLE'] == 'SADMIN' || response.session['ROLE'] == 'ADMIN') {
+
+					// add a button to delete coms, jquery 1.1 does not permit .live() function so we bind an onclick attribute
+					$('#'+currentComId).append('<button id="updatecom'+idcom+'" class="updateCom" onclick="fillCommentaire('+ idcom +')" >Modifier</button>'+
+											   '<button id="deletecom'+idcom+'" class="deleteCom" onclick="deleteCommentaire('+ idcom +')" >Supprimer</button>');
+				}
 			}
 			$('#divCom').append('</div>');
 			$('#textareaId').val('');
@@ -62,7 +72,6 @@ function getCommentaire(){
 			    	items: numItems,
 			    	itemsOnPage: perPage,
 			        onPageClick: function(pageNumber) { // this is where the magic happens
-			        	console.log(pageNumber);
 			            // someone changed page, lets hide/show trs appropriately
 			            var showFrom = perPage * (pageNumber - 1);
 			            var showTo = showFrom + perPage;
@@ -85,8 +94,50 @@ function addCommentaire(){
 		dataType: 'json',
 		data: {idPost: idArticle, content: content, role: "addCommentaire"},
 		success: function(response) {
-			console.log(response);
 			getCommentaire();
 		}
+	});
+}
+
+function deleteCommentaire(id){
+
+	$.ajax({
+		url: 'Ajax/comHandler.php',
+		type: 'POST',
+		dataType: 'json',
+		data: {idcom: id, role: "deleteCommentaire"},
+		success: function(response) {
+			getCommentaire();
+		}
+	});
+}
+
+function fillCommentaire(id){
+
+	var content = $('#content'+id).text();
+	$('.updateCom').remove();
+	$('.deleteCom').remove();
+	$('#content'+id).empty();
+	$('#content'+id).append('<textarea id="modifyTextarea"></textarea>');
+	$('#modifyTextarea').val(content);
+	$('#com'+id).append('<button id="validationUpdate" onclick="updateCommentaire('+id+')">Valider</button>');
+
+}
+
+function updateCommentaire(id){
+
+	var content = $('#modifyTextarea').val();
+
+	$.ajax({
+		url: 'Ajax/comHandler.php',
+		type: 'POST',
+		dataType: 'json',
+		data: {idcom: id, content: content, role: "updateCommentaire"},
+		success: function(response) {
+			getCommentaire();
+		}
+	}).done(function(response){
+		console.log(response);
+		console.log(response.session);
 	});
 }
