bf4480c6551d84e2fe44d09252c3ed48af01f70b
Cesar Hernandez Antonio <cesar19_fr@outlook.com>
2015-03-24 09:08:14 +0100

    Problème avec les images inexistants
    Mod -> MGenNewL.mod.php

--- Model/MGenNewL.mod.php
+++ Model/MGenNewL.mod.php
@@ -118,9 +118,9 @@ class MGenNewL extends FPDF
 			$this->Cell(0,5,utf8_decode("Association : ".$unique[$arrayID[$a]]),0,1,'C');
 			$this->Ln();
 			for ($b=0; $b < count($dataN); $b++) { 
-				if ($dataN[$b]['IMAGEPATH']=='' || $dataN[$b]['IMAGEPATH']==null) {
+				if ($dataN[$b]['IMAGEPATH']=='' || $dataN[$b]['IMAGEPATH']==null || !file_exists($dataN[$b]['IMAGEPATH'])) {
 					# Image
-					$this->Image('./Img/no-image.gif', $this->GetX(), $this->GetY()+3,30,30,'', 'http://dev.laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
+					$this->Image('./Img/no-image.gif', $this->GetX(), $this->GetY()+3,30,30,'', 'http://laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
 					# Space
 					$this->setX(47);
 					$this->Cell(0,5,"",'LRT',1,'L');
@@ -128,7 +128,7 @@ class MGenNewL extends FPDF
 					$this->setX(47);
 					$this->SetFont('Times','B',16);
 					$this->setTextColor(178,54,112);
-					$this->Cell(0,5,utf8_decode($dataN[$b]['TITLE']),'LR',2,'L',false, 'http://dev.laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
+					$this->Cell(0,5,utf8_decode($dataN[$b]['TITLE']),'LR',2,'L',false, 'http://laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
 				    # Author et Asso
 				    $this->setX(47);
 					$this->SetFont('Times','I',10);
@@ -147,7 +147,7 @@ class MGenNewL extends FPDF
 					$this->SetFillColor(163, 207, 234);
 					$this->SetFont('Times','B',11);
 					$this->setTextColor(12,80,145);
-					$this->Cell(0,5,utf8_decode("Afficher sur le site web ->").$this->Rect($this->GetX(), $this->GetY(), 45, 5, 'F'),'LR',1,'L',false,'http://dev.laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
+					$this->Cell(0,5,utf8_decode("Afficher sur le site web ->").$this->Rect($this->GetX(), $this->GetY(), 45, 5, 'F'),'LR',1,'L',false,'http://laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
 					$this->setX(47);
 					$this->Cell(0,5,"",'LRB',1,'L');
 					# Space Line
@@ -155,7 +155,7 @@ class MGenNewL extends FPDF
 					$this->Ln();
 				} else {
 					# Image
-					$this->Image($dataN[$b]['IMAGEPATH'], $this->GetX(), $this->GetY()+3,30,30,'', 'http://dev.laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
+					$this->Image($dataN[$b]['IMAGEPATH'], $this->GetX(), $this->GetY()+3,30,30,'', 'http://laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
 					# Space
 					$this->setX(47);
 					$this->Cell(0,5,"",'LRT',1,'L');
@@ -163,7 +163,7 @@ class MGenNewL extends FPDF
 					$this->setX(47);
 					$this->SetFont('Times','B',16);
 					$this->setTextColor(178,54,112);
-					$this->Cell(0,5,utf8_decode($dataN[$b]['TITLE']),'LR',2,'L',false, 'http://dev.laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
+					$this->Cell(0,5,utf8_decode($dataN[$b]['TITLE']),'LR',2,'L',false, 'http://laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
 				    # Author et Asso
 				    $this->setX(47);
 					$this->SetFont('Times','I',10);
@@ -182,7 +182,7 @@ class MGenNewL extends FPDF
 					$this->SetFillColor(163, 207, 234);
 					$this->SetFont('Times','B',11);
 					$this->setTextColor(12,80,145);
-					$this->Cell(0,5,utf8_decode("Afficher sur le site web ->").$this->Rect($this->GetX(), $this->GetY(), 45, 5, 'F'),'LR',1,'L',false,'http://dev.laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
+					$this->Cell(0,5,utf8_decode("Afficher sur le site web ->").$this->Rect($this->GetX(), $this->GetY(), 45, 5, 'F'),'LR',1,'L',false,'http://laplateformefne13.fr/index.php?EX=showInfoArticle&id='.$dataN[$b]['POST_ID']);
 					$this->setX(47);
 					$this->Cell(0,5,"",'LRB',1,'L');
 					# Space Line
